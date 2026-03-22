89548cbf04c795dd0674df4a7daf90f206e4c74d
DevAbdullah90 <abdullahkashif12b3@gmail.com>
2026-03-23 01:44:25 +0500

    feat: initial commit

---
+++ lib/sanity/queries.ts
@@ -0,0 +1,7 @@
+import { groq } from 'next-sanity'
+
+export const homePageQuery = groq`*[_type == "homepage"][0]`
+export const aboutPageQuery = groq`*[_type == "about"][0]`
+export const projectsQuery = groq`*[_type == "projects"] | order(publishedAt desc)`
+export const projectBySlugQuery = groq`*[_type == "projects" && slug.current == $slug][0]`
+export const siteSettingsQuery = groq`*[_type == "siteSettings"][0]`
