50958e9a3a153d4021b57dca1d0815c332e3c62e
DevAbdullah90 <abdullahkashif12b3@gmail.com>
2026-03-27 22:30:05 +0500

    query error solve

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -2,8 +2,8 @@ import { groq } from 'next-sanity'
 
 export const homePageQuery = groq`*[_type == "homepage"][0]`
 export const aboutPageQuery = groq`*[_type == "about"][0]`
-export const projectsQuery = groq`*[_type == "projects"] | order(publishedAt desc)`
-export const projectBySlugQuery = groq`*[_type == "projects" && slug.current == $slug][0]`
+export const projectsQuery = groq`*[_type == "project"] | order(publishedAt desc)`
+export const projectBySlugQuery = groq`*[_type == "project" && slug.current == $slug][0]`
 export const siteSettingsQuery = groq`*[_type == "siteSettings"][0]`
 export const announcementsQuery = groq`*[_type == "announcement" && active == true] | order(priority desc)`
 export const galleryQuery = groq`*[_type == "gallery"] | order(order asc, _createdAt desc)`
